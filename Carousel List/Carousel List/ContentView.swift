e67c056ba0cd6e6826a37bdab0c2c914ae32368d
Elaidzha1940 <eli080293@gmail.com>
2023-01-09 18:11:00 +0700

    SearchBar

--- Carousel List/Carousel List/ContentView.swift
+++ Carousel List/Carousel List/ContentView.swift
@@ -26,11 +26,32 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Eli: View {
+    
+    @State var search = ""
+    
     var body: some View {
         
         ScrollView(.vertical, showsIndicators: false) {
             
+            LazyHStack {
+                
+                HStack {
+                    Text("Movie Store")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                TextField("Search", text: self.$search)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(Color.black.opacity(0.07))
+                    .cornerRadius(12)
+            }
+            .padding(.vertical)
         }
+        .background(Color.black.opacity(0.05).edgesIgnoringSafeArea(.all))
     }
 }
 
@@ -44,13 +65,13 @@ struct Movie : Identifiable {
 
 var data = [
 
-    Game(id: 0, name: "Poker Face", image: "M0", rating: 4)
-    Game(id: 1, name: "Free State of Jones", image: "M1", rating: 2)
-    Game(id: 2, name: "News Of The World", image: "M2", rating: 6)
-    Game(id: 3, name: "The Women King", image: "M3", rating: 5)
-    Game(id: 4, name: "The Harder They Fall", image: "M4", rating: 3)
-    Game(id: 5, name: "Thirteen Lives", image: "M5", rating: 2)
-    Game(id: 6, name: "TILL", image: "M6", rating: 7)
-    Game(id: 7, name: "Emancipation", image: "M7", rating: 1)
+    Movie(id: 0, name: "Poker Face", image: "M0", rating: 4),
+    Movie(id: 1, name: "Free State of Jones", image: "M1", rating: 2),
+    Movie(id: 2, name: "News Of The World", image: "M2", rating: 6),
+    Movie(id: 3, name: "The Women King", image: "M3", rating: 5),
+    Movie(id: 4, name: "The Harder They Fall", image: "M4", rating: 3),
+    Movie(id: 5, name: "Thirteen Lives", image: "M5", rating: 2),
+    Movie(id: 6, name: "TILL", image: "M6", rating: 7),
+    Movie(id: 7, name: "Emancipation", image: "M7", rating: 1),
 ]
 
