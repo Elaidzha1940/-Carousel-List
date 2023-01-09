ed798b98fd185a78e4f683c65a39386e9bc5f865
Elaidzha1940 <eli080293@gmail.com>
2023-01-09 17:10:04 +0700

    data

--- Carousel List/Carousel List/ContentView.swift
+++ Carousel List/Carousel List/ContentView.swift
@@ -41,3 +41,16 @@ struct Movie : Identifiable {
     var image: String
     var rating: Int
 }
+
+var data = [
+
+    Game(id: 0, name: "", image: "", rating: 3)
+    Game(id: 1, name: "", image: "", rating: 1)
+    Game(id: 2, name: "", image: "", rating: 5)
+    Game(id: 3, name: "", image: "", rating: 7)
+    Game(id: 4, name: "", image: "", rating: 3)
+    Game(id: 5, name: "", image: "", rating: 4)
+    Game(id: 6, name: "", image: "", rating: 2)
+    Game(id: 7, name: "", image: "", rating: 5)
+]
+
