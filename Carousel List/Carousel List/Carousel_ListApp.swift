904f53093c954feabfaa6d7e1d939dda82cc6d31
Elaidzha1940 <eli080293@gmail.com>
2023-01-09 17:02:11 +0700

    Carousel List - Custom Grids And Animations

---
+++ Carousel List/Carousel List/Carousel_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_ListApp.swift
+//  Carousel List
+//
+//  Created by Elaidzha Shchukin on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carousel List/Carousel List/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  Carousel List
+//
+//  Created by Elaidzha Shchukin on 09.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+            ContentView()
+    }
+}
+
+struct Eli: View {
+    var body: some View {
+        
+        ScrollView(.vertical, showsIndicators: false) {
+            
+        }
+    }
+}
